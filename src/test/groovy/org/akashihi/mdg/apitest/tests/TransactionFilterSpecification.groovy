fd5eaf313ae8cffe512056f9f35c1bbbd1d0eb6d
Denis Chaplygin <dchaplyg@redhat.com>
2017-02-22 16:03:51 +0100

    Added tests for notEarlier/notLater

--- src/test/groovy/org/akashihi/mdg/apitest/tests/TransactionFilterSpecification.groovy
+++ src/test/groovy/org/akashihi/mdg/apitest/tests/TransactionFilterSpecification.groovy
@@ -5,6 +5,9 @@ import groovy.json.JsonOutput
 import org.akashihi.mdg.apitest.fixtures.TransactionFixture
 import spock.lang.*
 
+import java.time.LocalDate
+import java.time.LocalDateTime
+
 import static io.restassured.RestAssured.*
 import static org.akashihi.mdg.apitest.apiConnectionBase.setupAPI
 import static org.hamcrest.Matchers.*
@@ -40,6 +43,74 @@ class TransactionFilterSpecification extends Specification {
             current = x
         }
     }
+
+    def "User requests transactions after specified date"() {
+        given: "Several transactions with different dates"
+        f.makeTransactions()
+
+        when: "Transaction list filtered using notEarlier"
+        def response = given()
+                .queryParam("notEarlier", '2017-02-06T00:00:00')
+                .contentType("application/vnd.mdg+json").
+                when()
+                .get("/transaction")
+
+        then: "Every timestamp in returned list should be equal or greater then specified"
+        def body = JsonPath.parse(response.then()
+                .assertThat().statusCode(200)
+                .assertThat().contentType("application/vnd.mdg+json")
+                .extract().asString())
+        def ts = body.read("data[*].attributes.timestamp", List.class)
+        ts.each{ x ->
+            assertThat(LocalDateTime.parse(x).toLocalDate(), greaterThanOrEqualTo(LocalDate.parse("2017-02-06")))
+        }
+    }
+
+    def "User requests transactions before specified date"() {
+        given: "Several transactions with different dates"
+        f.makeTransactions()
+
+        when: "Transaction list filtered using notLater"
+        def response = given()
+                .queryParam("notLater", '2017-02-05T00:00:00')
+                .contentType("application/vnd.mdg+json").
+                when()
+                .get("/transaction")
+
+        then: "Every timestamp in returned list should be equal or less then specified"
+        def body = JsonPath.parse(response.then()
+                .assertThat().statusCode(200)
+                .assertThat().contentType("application/vnd.mdg+json")
+                .extract().asString())
+        def ts = body.read("data[*].attributes.timestamp", List.class)
+        ts.each{ x ->
+            assertThat(LocalDateTime.parse(x).toLocalDate(), lessThanOrEqualTo(LocalDate.parse("2017-02-04")))
+        }
+    }
+
+    def "User requests transactions between specified dates"() {
+        given: "Several transactions with different dates"
+        f.makeTransactions()
+
+        when: "Transaction list filtered using notLater"
+        def response = given()
+                .queryParam("notEarlier", '2017-02-05T00:00:00')
+                .queryParam("notLater", '2017-02-06T00:00:00')
+                .contentType("application/vnd.mdg+json").
+                when()
+                .get("/transaction")
+
+        then: "Every timestamp in returned list should be equal to specified"
+        def body = JsonPath.parse(response.then()
+                .assertThat().statusCode(200)
+                .assertThat().contentType("application/vnd.mdg+json")
+                .extract().asString())
+        def ts = body.read("data[*].attributes.timestamp", List.class)
+        ts.each{ x ->
+            assertThat(LocalDateTime.parse(x).toLocalDate(), is(LocalDate.parse("2017-02-05")))
+        }
+    }
+
 
     def "User sorts transaction on timestamp ascending"() {
         given: "Several transactions with different dates"
