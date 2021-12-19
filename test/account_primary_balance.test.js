bcd7b2e5c8758e5be19a0d53e1647ed628e809e8
Denis Chaplygin <akashihi@gmail.com>
2021-12-19 20:21:59 +0200

    TransactionMulticurrencySpecification refactored with Pactum.

---
+++ test/account_primary_balance.test.js
@@ -0,0 +1,55 @@
+const pactum = require('pactum');
+const {int, expression} = require('pactum-matchers');
+
+describe('Account primary balance', () => {
+    let e2e = pactum.e2e("Account primary balance")
+
+    it('Create account in non-default currency', async () => {
+        await e2e.step('Post account')
+            .spec('Create Account', {
+                '@DATA:TEMPLATE@': 'Account:Income',
+                '@OVERRIDES@': {
+                    data: {
+                        attributes: {
+                            currency_id: 840
+                        }
+                    }
+                }
+            })
+            .stores('IncomeAccountID', 'data.id')
+
+        await e2e.step('Post account')
+            .spec('Create Account', {
+                '@DATA:TEMPLATE@': 'Account:Asset',
+                '@OVERRIDES@': {
+                    data: {
+                        attributes: {
+                            currency_id: 840
+                        }
+                    }
+                }
+            })
+            .stores('AssetAccountID', 'data.id')
+    })
+
+    it('Transaction updates primary balance', async () => {
+
+        await e2e.step('Create transaction')
+            .spec('Create Transaction', {
+                '@DATA:TEMPLATE@': 'Transaction:Income'})
+
+        await e2e.step('Read account')
+            .spec('read')
+            .get("/account/{id}")
+            .withPathParams('id', '$S{IncomeAccountID}')
+            .expectJson('data.attributes.primary_balance', -133.5)
+
+        await e2e.step('Read account')
+            .spec('read')
+            .get("/account/{id}")
+            .withPathParams('id', '$S{AssetAccountID}')
+            .expectJson('data.attributes.primary_balance', 133.5)
+
+        await e2e.cleanup()
+    })
+})
---
+++ test/transaction_multicurrency.test.js
@@ -0,0 +1,32 @@
+const pactum = require('pactum');
+const {createAccountForTransaction} = require('./transaction.handler')
+
+describe('Transaction multi-currency operations', () => {
+    let e2e = pactum.e2e("Transaction multi-currency operations")
+
+    it('Create multi-currency transaction', async () => {
+        await createAccountForTransaction(e2e)
+
+        await e2e.step('Create USD Account')
+            .spec('Create Account', {'@DATA:TEMPLATE@': 'Account:AssetUSD'})
+            .stores('AssetUSDAccountID', 'data.id')
+
+        await e2e.step('Create transaction')
+            .spec('Create Transaction', {'@DATA:TEMPLATE@': 'Transaction:MultiCurrency'})
+            .stores('TransactionID', 'data.id')
+            .expectJsonLike("data.attributes.operations[*].amount", [-100, 200])
+            .expectJsonLike("data.attributes.operations[*].rate", [0.5])
+    })
+
+    it('Read multi-currency transaction', async () => {
+        await e2e.step('Read transaction')
+            .spec('read')
+            .get('/transaction/{id}')
+            .withPathParams('id', '$S{TransactionID}')
+            .expectJsonLike("data.attributes.operations[*].amount", [-100, 200])
+            .expectJsonLike("data.attributes.operations[*].rate", [0.5]);
+
+        await e2e.cleanup()
+    })
+
+})
