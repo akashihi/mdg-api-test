130ebd263060efb1ca1394a1e7f0afbe3fd017cb
Denis Chaplygin <akashihi@gmail.com>
2022-03-26 22:07:03 +0200

    Fixed rate list test for v0

--- test/v0/rate.test.js
+++ test/v0/rate.test.js
@@ -1,5 +1,5 @@
 const pactum = require('pactum');
-const { like } = require('pactum-matchers');
+const {like, expression} = require('pactum-matchers');
 const itParam = require('mocha-param');
 
 const USD = 840;
@@ -8,48 +8,40 @@ const INR = 356;
 const HUF = 348;
 
 it('Load list of rates', async () => {
-  await pactum.spec('read')
-    .get('/rate/{ts}')
-    .withPathParams('ts', '2017-09-20T13:29:00')
-    .expectJsonMatch('data[0]', {
-      type: 'rate',
-      id: like(1),
-      attributes: {
-        from_currency: EUR,
-        to_currency: USD,
-        rate: 1.19
-      }
-    });
+    await pactum.spec('read')
+        .get('/rate/{ts}')
+        .withPathParams('ts', '2017-09-20T13:29:00')
+        .expectJsonMatch('data[*].attributes.rate', expression('$S{AccountID}', '$V.includes(1.19)'));
 });
 
 const RATES = [
-  {
-    ratename: 'existing',
-    from: EUR,
-    to: USD,
-    rate: 1.19
-  },
-  {
-    ratename: 'non-existing',
-    from: INR,
-    to: HUF,
-    rate: 1
-  }
+    {
+        ratename: 'existing',
+        from: EUR,
+        to: USD,
+        rate: 1.19
+    },
+    {
+        ratename: 'non-existing',
+        from: INR,
+        to: HUF,
+        rate: 1
+    }
 ];
 
 itParam('Check ${value.ratename} rate value', RATES, async (params) => { // eslint-disable-line no-template-curly-in-string
-  await pactum.spec('read')
-    .get('/rate/{ts}/{from}/{to}')
-    .withPathParams('ts', '2017-09-20T13:29:00')
-    .withPathParams('from', params.from)
-    .withPathParams('to', params.to)
-    .expectJsonMatch('data', {
-      type: 'rate',
-      id: like(1),
-      attributes: {
-        from_currency: params.from,
-        to_currency: params.to,
-        rate: params.rate
-      }
-    });
+    await pactum.spec('read')
+        .get('/rate/{ts}/{from}/{to}')
+        .withPathParams('ts', '2017-09-20T13:29:00')
+        .withPathParams('from', params.from)
+        .withPathParams('to', params.to)
+        .expectJsonMatch('data', {
+            type: 'rate',
+            id: like(1),
+            attributes: {
+                from_currency: params.from,
+                to_currency: params.to,
+                rate: params.rate
+            }
+        });
 });
